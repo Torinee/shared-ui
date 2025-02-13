abb872c9fe1c920beac77db963cf1a0571264084
Tran Minh Tri <tri.tran@navigosgroup.com>
2025-02-13 14:44:54 +0700

    Update Container

--- src/components/TextArea.tsx
+++ src/components/TextArea.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 import styled from 'styled-components'
 
-const Container = styled.div``
+const Container = styled.div`
+    width: '100%';
+    height: 300;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
 
 const TextArea = () => {
     return <Container>TextArea</Container>
